abdbd8dcd6a2f92d2cfdb39e6428835fadfaca86
안정근 <ahn@anjeong-geun-ui-MacBookPro.local>
2019-07-25 15:22:05 +0900

    CLEAR BK 1018

---
+++ BaekJoon/1018_체스판 다시 칠하기.cpp
@@ -0,0 +1,68 @@
+
+#include<stdio.h>
+char comp[] = {'W', 'B'}; // compare character
+int start = 0; // start is W or B 
+int row = 0; 
+int col = 0; // (row, col) start point
+int min = 65; // changed Minimum
+
+int main(void)
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+
+    char h[n][m];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%s", h[i]);
+    }
+
+    while(row + 8 <= n && col + 8 <= m)
+    {
+        for(int k=0; k<2; k++)
+        {
+            int cnt = 0;
+            start = k;
+            for(int i=row; i<row+8; i++)
+            {
+                for(int j=col; j<col+8; j++)
+                {
+                    if(h[i][j] != comp[start % 2])
+                    {
+                        cnt += 1;
+                    }
+                    start += 1;
+                }
+                start += 1;
+            }
+            if(cnt < min)
+            {
+                min = cnt;
+            }
+        }
+
+        col += 1;
+        if(col + 8 > m)
+        {
+            col = 0;
+            row += 1;
+        }
+    }
+
+    printf("%d", min);
+    return 0;
+}
+
+/*
+8 8
+BBBBWBBW
+WWWWBBWB
+WWBBWBWW
+WBWWBWBW
+WBBWBBWB
+BWBWBWWB
+BWWWWWBW
+BWBBBBWW
+답 : 29
+출력 : 35
+ */
