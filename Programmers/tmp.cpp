7fa3941ec56019e82087e8a5b3fb3fe2ff110371
AhnJG <arki13@naver.com>
2019-10-01 16:30:10 +0900

    add program/tmp

---
+++ Programmers/tmp.cpp
@@ -0,0 +1,88 @@
+/*
+문제 설명
+함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+제한 조건
+n은 1이상 8000000000 이하인 자연수입니다.
+입출력 예
+n	return
+118372	873211
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long ans = 0;
+    vector<int> v;
+    while(n)
+    {
+        v.push_back(n%10);
+        n = n/10;
+    }
+    
+    sort(v.begin(), v.end());
+    
+    for(int i=1; i<v.size()+1; i++)
+    {
+        ans *= 10;
+        ans += v[v.size()-i];
+    }
+    
+    return ans;
+}
+
+/*
+문제 설명
+array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+
+제한사항
+arr은 자연수를 담은 배열입니다.
+정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
+divisor는 자연수입니다.
+array는 길이 1 이상인 배열입니다.
+입출력 예
+arr	divisor	return
+[5, 9, 7, 10]	5	[5, 10]
+[2, 36, 1, 3]	1	[1, 2, 3, 36]
+[3,2,6]	10	[-1]
+입출력 예 설명
+입출력 예#1
+arr의 원소 중 5로 나누어 떨어지는 원소는 5와 10입니다. 따라서 [5, 10]을 리턴합니다.
+
+입출력 예#2
+arr의 모든 원소는 1으로 나누어 떨어집니다. 원소를 오름차순으로 정렬해 [1, 2, 3, 36]을 리턴합니다.
+
+입출력 예#3
+3, 2, 6은 10으로 나누어 떨어지지 않습니다. 나누어 떨어지는 원소가 없으므로 [-1]을 리턴합니다.
+*/
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    bool chk = true;
+    
+    for(int i=0; i<arr.size(); i++)
+    {
+        if(arr[i] % divisor == 0)
+        {
+            answer.push_back(arr[i]);
+            chk = false;
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    if(chk)
+    {
+        answer.push_back(-1);
+    }
+    return answer;
+}
