c2a0e9ccee2412629a86a43c54d1952d5e0f4089
AhnJG <arki13@naver.com>
2020-06-07 18:45:58 +0900

    solve 11718

---
+++ BaekJoon/11718_그대로 출력하기.py
@@ -0,0 +1,16 @@
+# method 1
+# import sys
+
+# for line in sys.stdin:
+#     print(line, end='')
+
+# method 2
+# while True:
+#     try:
+#         print(input())
+#     except EOFError:
+#         break
+
+# method 3
+# import sys
+# print(sys.stdin.read())
