48390999a7d4531721491add154d8d23bee1a84d
AhnJG <arki13@naver.com>
2020-06-09 19:18:41 +0900

    solve 11726

---
+++ BaekJoon/DP/11726_2N타일링.py
@@ -0,0 +1,20 @@
+# method 1
+dp = {1:1, 2:2}
+
+def find(n):
+    if n in dp:
+        return dp[n]
+    
+    m = find(n-1) + find(n-2)
+    dp[n] = m
+    return m
+
+n = int(input())
+print(find(n) % 10007)
+
+# method 2
+n = int(input())
+a = b = 1
+for i in range(n):
+    a, b = b, a+b
+print(a%10007)
