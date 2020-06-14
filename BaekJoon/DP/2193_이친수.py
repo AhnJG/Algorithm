fe1d67bafdc04a6051a25694d5779db2dadd861b
AhnJG <arki13@naver.com>
2020-06-14 18:43:41 +0900

    mod 2193

--- BaekJoon/DP/2193_이친수.py
+++ BaekJoon/DP/2193_이친수.py
@@ -4,10 +4,7 @@ dp = [[0] * 2 for i in range(n+1)]
 dp[1] = [0, 1]
 
 for i in range(2, n+1):
-    for j in range(2):
-        if j == 0:
-            dp[i][0] = dp[i-1][0] + dp[i-1][1]
-        elif j == 1:
-            dp[i][1] = dp[i-1][0]
+    dp[i][0] = dp[i-1][0] + dp[i-1][1]
+    dp[i][1] = dp[i-1][0]
         
 print(sum(dp[n]))
